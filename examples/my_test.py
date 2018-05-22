7394e2d902f974c2ee6e7f3dd0a056e9a2cee6b5
George T. Chatzikonstantis <georgec@microlab.ntua.gr>
2018-05-22 16:37:18 +0300

    Updated the examples/my_test.py script

--- examples/my_test.py
+++ examples/my_test.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python 
 
 import sys
-sys.path.insert(0, '/home/georgec/docker_playground/pyNN_dev')
+sys.path.insert(0, '/app/dockerHHext')
 print(sys.path)
 import pyNN.brainframe as sim
 
+print("Setup...")
 sim.setup()
-pop=sim.Population(96, sim.HH_cond_exp(i_offset=0.6 ), label="HH_cond_exp")
+print("Setup Done...")
+pop=sim.Population(960, sim.HH_cond_exp(i_offset=0.6 ), label="HH_cond_exp")
 syn = sim.StaticSynapse(weight=0.04, delay=0)
 prj = sim.Projection(pop, pop, sim.FixedProbabilityConnector(0.25, allow_self_connections=False), syn)
+print("Network Built...")
+
+sele = sim.Backend_selector(prj,'XEON')
+selected = sele.select_backend()
+corw = sim.Sim_core(selected,prj,'/app/dockerHHext/pyNN/brainframe/sim_core.ini')
+print("Xeon Backend Selected...")
+print("Running...")
+corw.run_sim_core(simtime=1000)
+
 sele = sim.Backend_selector(prj,'PHI')
 selected = sele.select_backend()
-corw = sim.Sim_core(selected,prj,'/home/georgec/docker_playground/pyNN_dev/pyNN/brainframe/sim_core.ini')
-corw.run_sim_core(simtime=100,run_id='0dmhCK6GhYp_haSqAAAO')
+corw = sim.Sim_core(selected,prj,'/app/dockerHHext/pyNN/brainframe/sim_core.ini')
+print("Phi Backend Selected...")
+print("*THIS WILL PROBABLY CRUSH*")
+print("Running...")
+corw.run_sim_core(simtime=1000)
